1f1fa3b6c1220f47b8818a5f7ca75344befd9ec7
Jesper D. S. Jensen <jdsj@Jespers-MacBook-Pro.local>
2024-12-20 13:02:32 +0100

    ups

--- src/main/java/dk/jdsj/battlenskae/BattlenskaeApplication.java
+++ src/main/java/dk/jdsj/battlenskae/BattlenskaeApplication.java
@@ -1,9 +1,14 @@
 package dk.jdsj.battlenskae;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(
+        servers = @Server(url = "https://bs-api.desperate.dk")
+)
 @SpringBootApplication
 @EnableJpaRepositories
 public class BattlenskaeApplication {
